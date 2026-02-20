44873293e852bd2be7a888dbeb26563c6961dd28
JAHID <dev.jahidulislam007@gmail.com>
2026-02-20 15:35:44 +0600

    add counter files

---
+++ script.js
@@ -0,0 +1,39 @@
+let count = 0;
+loadCount();
+updateDisplay();
+function updateDisplay() {
+  document.getElementById("count").innerText = count;
+}
+
+function increaseCount() {
+  count++;
+  updateDisplay();
+}
+
+function decreaseCount() {
+  if (count > 0) {
+    count--;
+    updateDisplay();
+  }
+}
+
+function resetCount() {
+  count = 0;
+  updateDisplay();
+}
+
+function saveCount() {
+  localStorage.setItem("savedCount", count);
+  alert("Count saved!");
+}
+
+function loadCount() {
+  const saved = localStorage.getItem("savedCount");
+  if (saved !== null) {
+    count = parseInt(saved);
+    updateDisplay();
+    alert("Count loaded!");
+  } else {
+    alert("No saved count found.");
+  }
+}
